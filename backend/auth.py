ac886aaadc9ec2064854b44bcd87cd68658e88e9
SevenErol <lorenzo.maggi1998@gmail.com>
2024-05-07 18:08:19 +0200

    installed package for react frontend

--- backend/auth.py
+++ backend/auth.py
@@ -2,7 +2,7 @@ from flask import request, jsonify, make_response
 from flask_restx import Resource, Namespace, fields
 from models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 
 auth_ns = Namespace("auth", description = "A namespace for our Authentication")
 
@@ -74,3 +74,13 @@ class Login(Resource):
             return make_response( jsonify(
                 {"access_token": access_token, "refresh_token": refresh_token}
             ), 200)
+
+@auth_ns.route('/refresh')
+class RefreshResource(Resource):
+    @jwt_required(refresh = True)
+
+    def post(self):
+        current_user = get_jwt_identity()
+        new_access_token = create_access_token(identity = current_user)
+
+        return make_response(jsonify({"access_token": new_access_token}), 200)
